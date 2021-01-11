3e8bbc19d383b25551bbd160b01dbbe3e15b76e7
SahenJavac <54188335+SahenJavac@users.noreply.github.com>
2021-01-11 17:50:43 +0530

    Add files via upload

---
+++ Exception Handling/ArmyEnrolment.java
@@ -0,0 +1,91 @@
+
+class CountryValidationException extends Exception {
+
+    CountryValidationException(String s) {
+        super(s);
+    }
+}
+
+class HeightValidationException extends Exception {
+
+    HeightValidationException(String s) {
+        super(s);
+    }
+}
+
+class WeightValidationException extends Exception {
+
+    WeightValidationException(String s) {
+        super(s);
+    }
+}
+
+class AgeValidationException extends Exception {
+
+    AgeValidationException(String s) {
+        super(s);
+    }
+}
+
+public class ArmyEnrolment {
+
+    static void heightValidation(int height) throws HeightValidationException {
+        if (height >= 150) {
+
+        } else {
+            throw new HeightValidationException("You are too short");
+        }
+    }
+
+    static void countryValidation(String country) throws CountryValidationException {
+        if (!country.equals("Sri Lanka")) {
+            throw new CountryValidationException("Country validation failed");
+        } else {
+            try {
+                ageValidation(23);
+            } catch (AgeValidationException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    static void weightValidation(int weight) throws WeightValidationException {
+        if (weight>50) {
+            try {
+                heightValidation(120);
+            } catch (HeightValidationException ex) {
+                System.out.println(ex);
+                ex.printStackTrace();
+            }
+        } else {
+            throw new WeightValidationException("You need to gain weight");
+        }
+    }
+
+    static void ageValidation(int age) throws AgeValidationException {
+        if (age >= 18) {
+            try {
+                weightValidation(60);
+            } catch (WeightValidationException e) {
+                System.out.println(e);
+            }
+        } else {
+            throw new AgeValidationException("You are too young");
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            countryValidation("Sri Lanka");
+        } catch (Exception e) {
+            System.out.println("Excption " + e);
+        }
+
+
+
+    }
+
+}
+
+
+
